1390f3477f895c8878b64f9b958d5d9261de6a48
Tobias McNulty <tobias@caktusgroup.com>
2011-11-04 13:54:07 -0400

    switch to default html_theme for sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -121,21 +121,21 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
+html_theme = 'default'
 #html_theme = 'proBlue'
 #html_theme_path = ['.']
 
-html_theme = 'agogo'
+#html_theme = 'agogo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'textalign': 'left',
-    'documentwidth': '650px',
-    'sidebarwidth': '240px',
-    'pagewidth': '900px',
-}
+#html_theme_options = {
+#    'textalign': 'left',
+#    'documentwidth': '650px',
+#    'sidebarwidth': '240px',
+#    'pagewidth': '900px',
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
